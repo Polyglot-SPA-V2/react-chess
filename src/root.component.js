21d823bf282eeb3d126f3b16a29d4719db7e8b62
Eric Eugene Carter <eric.eugene.carter@gmail.com>
2024-03-21 10:25:36 -0400

    Add azure-pipelines.yml, update root component to have link to angular mfe

--- src/root.component.js
+++ src/root.component.js
@@ -1,3 +1,8 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <>
+      <section>{props.name} is mounted!</section>
+      <a href={"/angular-chess"}>Angular Chess</a>
+    </>
+  );
 }
